542e84ab54415554b16d262dbaff8d1c151aa3b6
LighthouseBoy <30687@longroad.ac.uk>
2013-10-14 09:51:05 +0100

    manz don dis 4 dem dyslexia awareness week innit fam

---
+++ Revision ex 2.py
@@ -0,0 +1,4 @@
+message = input("what's your message?: ")
+timesdisplayed = int(input("how many times do you want the message to be displayed?: "))
+for counter in range (timesdisplayed):
+    print (message)
